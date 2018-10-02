4bf55cfeb6b366ab428a50753745fc9f94dcfcd0
Rafał Majkowski <majkipl@gmail.com>
2018-10-02 08:29:33 +0200

    add layout add media

---
+++ resources/views/common/footer.blade.php
@@ -0,0 +1,26 @@
+<footer class="pb-5 pt-4 text-center text-md-left">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-7 col-xl-8 mt-4">
+                <a href="/" class="d-block" title="STRONA GŁÓWNA">
+                    <img src="{{ asset('images/logo-footer-compressor.png') }}" alt="Russell Hobbs" />
+                </a>
+            </div>
+            <div class="col-12 col-md-3 col-lg-3 col-xl-2 mt-4 footer-menu">
+                <a href="{{ asset('uploads/polityka.pdf') }}" title="Polityka prywatności" class="float-left" target="_blank">Polityka prywatności</a>
+                <a href="{{ asset('uploads/regulamin.pdf') }}" title="Regulamin" class="float-left" target="_blank">Regulamin</a>
+            </div>
+            <div class="col-12 col-md-3 col-lg-2 mt-4 text-center socials">
+                <a href="https://www.facebook.com/RussellHobbsPolska" title="facebook.com" class="fb float-lg-right ml-2" target="_blank">
+                    <i class="fab fa-facebook-f fa-md"></i>
+                </a>
+                <a href="https://www.instagram.com/kochamdom/" title="instagram.com" class="fb float-lg-right ml-2" target="_blank">
+                    <i class="fab fa-instagram fa-lg"></i>
+                </a>
+                <a href="https://www.youtube.com/user/russellhobbspolska" title="youtube.com" class="yt float-lg-right ml-2" target="_blank">
+                    <i class="fab fa-youtube fa-lg"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/common/top.blade.php
@@ -0,0 +1,41 @@
+<section class="top" id="top">
+
+    <div class="container py-0 container-navbar-menu">
+        <nav class="navbar navbar-light">
+            <a class="navbar-brand" href="/">
+                <img src="{{ asset('images/logo-compressor.png') }}" alt="Russell Hobbs" />
+            </a>
+            <button class="hamburger hamburger--collapse float-right" type="button">
+			  <span class="hamburger-box">
+				<span class="hamburger-inner"></span>
+			  </span>
+            </button>
+        </nav>
+    </div>
+
+    <div class="container-menu">
+        <div class="container">
+            <nav class="menu text-right">
+                <div class="navbar-collapse navbar-menu" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto text-uppercase">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/zasady/" data-href="#rules">ZASADY PROMOCJI</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/formularz" data-href="#formularz">FORMULARZ ZGŁOSZENIOWY</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/produkty/" data-href="#products">PRODUKTY PROMOCYJNE</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/kontakt/" data-href="#contact">KONTAKT</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ asset('uploads/regulamin.pdf') }}" data-href="#regulamin" target="_blank">REGULAMIN</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+</section>
