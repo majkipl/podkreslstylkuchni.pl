539f9e12a7f9fcea629150653927867a57459fcb
Rafał Majkowski <majkipl@gmail.com>
2018-10-04 12:29:14 +0200

    modify for error 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.front')
+
+@section('content')
+    <section class="e404" id="e404">
+        <div class="container text-center d-flex flex-column align-content-center justify-content-center">
+            <h1>404</h1>
+        </div>
+    </section>
+@endsection
